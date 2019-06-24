ae551a651f89eeea455814458442b5060fbfbc43
Suelen Costa Raimundo <suelen.costa@meta.com.br>
2019-06-24 17:21:51 -0300

    melhorando um pouco

--- projeto/testes/index.php
+++ projeto/testes/index.php
@@ -52,7 +52,7 @@ if(isset($_POST['time'])&& isset($_POST['resp'])){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
 </head>
-<body>
+<body class="telas">
 
 <div class="container">    
 <form method="POST" action="index.php">
